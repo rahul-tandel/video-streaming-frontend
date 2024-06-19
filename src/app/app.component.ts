e80359bb291e8b42c1d4b5696d993232dbe4f68a
Divya Gupta <yash@stuzlzservice.in>
2024-06-19 18:01:43 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,9 @@ import { Socket, io } from 'socket.io-client'
 export class AppComponent implements AfterViewInit {
   title = 'shared-video';
   private socket!: Socket
+  public video: HTMLMediaElement | null | HTMLElement = document.getElementById('video')
+  public userSeeked = true
+  public time = 0
 
   constructor() {
     this.socket = io('http://localhost:3000')
@@ -18,22 +21,27 @@ export class AppComponent implements AfterViewInit {
 
 
   ngAfterViewInit(): void {
-    this.socket.emit('seek', 60)
+    // this.socket.emit('seek', 60)
     const video: HTMLMediaElement | null | HTMLElement = document.getElementById('video')
     const videoSrc = 'https://firebasestorage.googleapis.com/v0/b/video-streaming-e4f77.appspot.com/o/71d24b0f-f609-40bd-9741-6f57c69734b6%2Foutput.m3u8?alt=media'
+    this.video = video
     if (video && video instanceof HTMLMediaElement) {
       if (Hls.isSupported()) {
         var hls = new Hls();
         hls.loadSource(videoSrc);
         hls.attachMedia(video);
         this.socket.on('seek', (time) => {
-          video.currentTime = time
+          video.currentTime = time.time
+          this.time = time.time
+          console.log(this.userSeeked)
+          this.userSeeked = !time.userSeeked
         })
         this.socket.on('play', () => {
           video.muted = true
           video.play()
         })
         this.socket.on('pause', () => {
+
           console.log('pause')
           video.pause()
         })
@@ -51,14 +59,29 @@ export class AppComponent implements AfterViewInit {
 
   onPause() {
     this.socket.emit('pause')
+
   }
 
   onplay() {
-    this.socket.emit('play')
+    this.socket.emit('play-client')
   }
 
   handelChange() {
-    console.log('change triggered')
+    console.log(this.userSeeked)
+    if (this.video && this.video instanceof HTMLMediaElement && this.userSeeked) {
+      this.socket.emit('seek', this.video.currentTime, true)
+
+    }
+  }
+
+  updateTime() {
+    console.log(this.userSeeked)
+    if (this.video && this.video instanceof HTMLMediaElement) {
+      if (this.video.currentTime > +this.time + 5) {
+        this.userSeeked = true
+      }
+    }
+
   }
 
 }
